9f1a299ec6604bee374b99b1d48890fd731745f0
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-09-04 17:52:12 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
+@app.route("/web")
+def web():
     return """<!doctype html> 
         <html>
            <body> 
-               <h1>web-сервер на flask</h1> 
+               <h1>web-сервер на flask</h1>
+               <a href="/author">author</a> 
            <body> 
         </html>"""
+
+@app.route("/author")
+def author():
+   name="Орлова Ярослава Владиславовна"
+   group="ФБИ-22"
+   faculty="ФБ"
+   
+   return """<!doctype html> 
+        <html>
+           <body> 
+               <p>Судент: """ +name+ """</p>
+               <p>Группа: """ +group+ """</p>
+               <p>Факультет: """ +faculty+ """</p>
+               <a href="/web">web</a> 
+           <body> 
+        </html>"""    
