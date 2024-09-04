f4dba8e113c748af23057598de79fc40d137b51a
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-09-04 18:10:20 +0700

    Дуб

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/web")
@@ -25,4 +25,16 @@ def author():
                <p>Факультет: """ +faculty+ """</p>
                <a href="/web">web</a> 
            <body> 
-        </html>"""    
+        </html>"""
+
+@app.route("/lab1/oak") 
+def oak(): 
+    path = url_for("static", filename="oak.jpg") 
+    return ''' 
+<!doctype html> 
+<html> 
+    <body> 
+        <h1>Дуб</h1> 
+        <img src="''' + path + '''"> 
+    </body> 
+</html>'''    
