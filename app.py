59acc155c5560c64fb57800e9d84e0299d5887b3
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-09-17 16:41:39 +0700

    Список роутов

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def index():
                     background-color: #f8f8f8;
                     padding: 10px;
                     text-align: center;
-                    margin-top: auto; /* Это свойство прижимает футер к низу */
+                    margin-top: auto;
                 }
                 nav {
                     padding: 20px;
@@ -62,25 +62,63 @@ def index():
     </html>
     """
 
-# Обновлённый маршрут /lab1/web
+# Маршрут /lab1
+@app.route("/lab1")
+def lab1():
+    return """
+    <!doctype html>
+    <html>
+        <head>
+            <title>Лабораторная 1</title>
+        </head>
+        <body>
+            <h1>Лабораторная 1</h1>
+            <p>Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, 
+            а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, 
+            сознательно предоставляющих лишь самые базовые возможности.</p>
+         
+
+            <h2>Список роутов</h2>
+            <ul>
+                <li><a href="/lab1/web">Главная страница лабораторной (/lab1/web)</a></li>
+
+            </ul>
+
+            <a href="/">Вернуться на главную</a>
+        </body>
+    </html>
+    """
+
+# Маршрут /lab1/web с полным списком всех роутов
 @app.route("/lab1/web")
 def web():
-    return """<!doctype html> 
+    return """<!doctype html>
         <html>
-           <body> 
-               <h1>web-сервер на flask</h1>
+           <body>
+               <h1>WEB-сервер на Flask — все роуты</h1>
                <ul>
-                   <li><a href="/lab1/author">Автор</a></li>
-                   <li><a href="/lab1/oak">Страница с дубом</a></li>
-                   <li><a href="/lab1/counter">Счётчик посещений</a></li>
-                   <li><a href="/lab1/info">Перенаправление на автора (Info)</a></li>
-                   <li><a href="/lab1/create">Код ответа 201 (Create)</a></li>
-                   <li><a href="/lab1/response_headers">HTTP заголовки</a></li>
+                   <li><a href="/lab1/author">Страница автора (/lab1/author)</a></li>
+                   <li><a href="/lab1/oak">Страница с дубом (/lab1/oak)</a></li>
+                   <li><a href="/lab1/counter">Счётчик посещений (/lab1/counter)</a></li>
+                   <li><a href="/lab1/reset_counter">Сброс счётчика (/lab1/reset_counter)</a></li>
+                   <li><a href="/lab1/info">Перенаправление на автора (/lab1/info)</a></li>
+                   <li><a href="/lab1/create">Код ответа 201 (Created) (/lab1/create)</a></li>
+                   <li><a href="/lab1/response_headers">HTTP заголовки (/lab1/response_headers)</a></li>
+                   <li><a href="/lab1/error400">Ошибка 400: Bad Request (/lab1/error400)</a></li>
+                   <li><a href="/lab1/error401">Ошибка 401: Unauthorized (/lab1/error401)</a></li>
+                   <li><a href="/lab1/error402">Ошибка 402: Payment Required (/lab1/error402)</a></li>
+                   <li><a href="/lab1/error403">Ошибка 403: Forbidden (/lab1/error403)</a></li>
+                   <li><a href="/lab1/error405">Ошибка 405: Method Not Allowed (/lab1/error405)</a></li>
+                   <li><a href="/lab1/error418">Ошибка 418: I'm a teapot (/lab1/error418)</a></li>
+                   <li><a href="/lab1/error500">Ошибка 500: Internal Server Error (/lab1/error500)</a></li>
+                   <li><a href="/lab1/thewitcher">Страница про The Witcher (/lab1/thewitcher)</a></li>
+                   <li><a href="/nonexistent">Искусственная ошибка 404 (/nonexistent)</a></li>
                </ul>
-           </body> 
+               <a href="/">Вернуться на главную</a>
+           </body>
         </html>"""
 
-# Обновлённый маршрут /lab1/author
+# Страница автора
 @app.route("/lab1/author")
 def author():
     name = "Орлова Ярослава Владиславовна"
@@ -93,7 +131,7 @@ def author():
                <p>Судент: """ + name + """</p>
                <p>Группа: """ + group + """</p>
                <p>Факультет: """ + faculty + """</p>
-               <a href="/lab1/web">Вернуться на главную</a> 
+               <a href="/lab1/web">Вернуться ко всем роутам</a>
            </body> 
         </html>"""
 
@@ -111,7 +149,7 @@ def oak():
     <body> 
         <h1>Дуб</h1> 
         <img src="''' + image_path + '''"> 
-        <a href="/lab1/web">Вернуться на главную</a> 
+        <a href="/lab1/web">Вернуться ко всем роутам</a>
     </body> 
 </html>'''    
 
@@ -127,7 +165,7 @@ def counter():
             <h1>Счётчик посещений</h1>
             <p>Эта страница была посещена """ + str(count) + """ раз(а).</p>
             <a href="/lab1/reset_counter">Очистить счётчик</a><br>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """
@@ -142,15 +180,16 @@ def reset_counter():
     <html>
         <body>
             <h1>Счётчик был очищен!</h1>
-            <a href="/lab1/counter">Вернуться на страницу счётчика</a>
+            <a href="/lab1/counter">Вернуться на страницу счётчика</a><br>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """
 
-# Обновлённый маршрут /lab1/info
+# Перенаправление на автора
 @app.route("/lab1/info")
 def info():
-    return redirect(url_for('author'))  # Перенаправление на страницу /lab1/author
+    return redirect(url_for('author'))
 
 # Код 201 "Created"
 @app.route("/lab1/create")
@@ -161,15 +200,15 @@ def create():
         <body>
             <h1>Ресурс был создан!</h1>
             <p>Этот ответ возвращает код 201 (Created).</p>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
-    """, 201  # Указываем код ответа 201
+    """, 201
 
 # Обработчик ошибки 404 с добавленными стилями и изображением
 @app.errorhandler(404)
 def page_not_found(e):
-    image_path = url_for("static", filename="404_image.jpg")  # Предположим, что у тебя есть изображение "404_image.jpg" в папке static
+    image_path = url_for("static", filename="404_image.jpg")
     return """
     <!doctype html>
     <html>
@@ -214,13 +253,18 @@ def page_not_found(e):
         </head>
         <body>
             <h1>404: Страница не найдена</h1>
-            <p>Извините, но страница, которую вы ищете, не существует. Возможно, она была перемещена или удалена.</p>
-            <p>Не волнуйтесь, вы всегда можете вернуться на <a href="/lab1/web">главную страницу</a>.</p>
+            <p>Извините, но страница, которую вы ищете, не существует.</p>
+            <p>Не волнуйтесь, вы всегда можете вернуться на <a href="/lab1/web">главную страницу роутов</a>.</p>
             <img src='""" + image_path + """' alt="404 error image">
         </body>
     </html>
     """, 404
 
+# Искусственная ошибка 404
+@app.route("/nonexistent")
+def nonexistent():
+    # Специальный маршрут, который вызывает ошибку 404
+    return page_not_found(None)
 
 # Перехватчик для ошибки 500
 @app.errorhandler(500)
@@ -266,18 +310,10 @@ def internal_server_error(e):
         <body>
             <h1>500: Внутренняя ошибка сервера</h1>
             <p>Произошла внутренняя ошибка на сервере. Мы уже работаем над её устранением.</p>
-            <p>Пожалуйста, попробуйте зайти позже или вернитесь на <a href="/lab1/web">главную страницу</a>.</p>
+            <p>Пожалуйста, попробуйте зайти позже или вернитесь на <a href="/lab1/web">главную страницу роутов</a>.</p>
         </body>
     </html>
     """, 500
-
-
-
-@app.route("/lab1/error500")
-def trigger_error():
-    result = 1 / 0  # Это вызовет ZeroDivisionError
-    return str(result)
-
 
 # HTTP заголовки
 @app.route("/lab1/response_headers")
@@ -286,33 +322,12 @@ def response_headers():
         <html>
            <body> 
                <h1>Заголовки ответа HTTP</h1>
-               <a href="/lab1/web">Вернуться на главную</a>
+               <a href="/lab1/web">Вернуться ко всем роутам</a>
            </body> 
         </html>""", 200, {
         'X-Server': 'sample',
         'Content-Type': 'text/plain; charset=utf-8'
     }
-
-# Новый маршрут /lab1
-@app.route("/lab1")
-def lab1():
-    return """
-    <!doctype html>
-    <html>
-        <head>
-            <title>Лабораторная 1</title>
-        </head>
-        <body>
-            <h1>Лабораторная 1</h1>
-            <p>Flask — фреймворк для создания веб-приложений на языке программирования Python, использующий набор инструментов Werkzeug, 
-            а также шаблонизатор Jinja2. Относится к категории так называемых микрофреймворков — минималистичных каркасов веб-приложений, 
-            сознательно предоставляющих лишь самые базовые возможности.</p>
-            <ul><a href="/lab1/web">Задания первой лабораторной</a></ul>
-            <a href="/">Вернуться на главную</a>
-        </body>
-    </html>
-    """
-
 
 # Страница с кодом 400 (Bad Request)
 @app.route("/lab1/error400")
@@ -323,7 +338,7 @@ def error400():
         <body>
             <h1>400: Bad Request</h1>
             <p>Запрос не может быть выполнен из-за неверного синтаксиса.</p>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """, 400
@@ -337,7 +352,7 @@ def error401():
         <body>
             <h1>401: Unauthorized</h1>
             <p>Для доступа к запрашиваемому ресурсу требуется аутентификация.</p>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """, 401
@@ -351,7 +366,7 @@ def error402():
         <body>
             <h1>402: Payment Required</h1>
             <p>Требуется оплата для доступа к ресурсу.</p>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """, 402
@@ -365,7 +380,7 @@ def error403():
         <body>
             <h1>403: Forbidden</h1>
             <p>У вас нет прав для доступа к запрашиваемому ресурсу.</p>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """, 403
@@ -379,7 +394,7 @@ def error405():
         <body>
             <h1>405: Method Not Allowed</h1>
             <p>Метод, используемый в запросе, не поддерживается для запрашиваемого ресурса.</p>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """, 405
@@ -393,13 +408,12 @@ def error418():
         <body>
             <h1>418: I'm a teapot</h1>
             <p>Я - чайник. Я не могу заварить кофе, потому что я чайник.</p>
-            <a href="/lab1/web">Вернуться на главную</a>
+            <a href="/lab1/web">Вернуться ко всем роутам</a>
         </body>
     </html>
     """, 418
 
-
-# Маршрут с текстом и заголовками
+# Страница с текстом про The Witcher
 @app.route("/lab1/thewitcher")
 def thewitcher_page():
     image_path = url_for("static", filename="custom_image.jpg")
@@ -462,8 +476,7 @@ def thewitcher_page():
         </body>
     </html>
     """, 200, {
-        'Content-Language': 'ru',  # Указывает, что страница на русском языке
-        'X-Custom-Header-One': 'Witcher Universe',  # Пользовательский заголовок
-        'X-Custom-Header-Two': 'Fantasy Saga'       # Ещё один пользовательский заголовок
+        'Content-Language': 'ru',
+        'X-Custom-Header-One': 'Witcher Universe',
+        'X-Custom-Header-Two': 'Fantasy Saga'
     }
-
