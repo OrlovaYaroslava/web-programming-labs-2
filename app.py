a2e56dac23dbd5014e3bff5791426747e06477ae
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-09-24 16:46:05 +0700

    Фильтры

--- app.py
+++ app.py
@@ -458,4 +458,8 @@ def lab2():
     return render_template('lab2.html')
 
 
+@app.route('/lab2/filters')
+def filters():
+    phrase = "О сколько нам открытий чудных..."
+    return render_template('filter.html', phrase=phrase)
 
