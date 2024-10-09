d594ddd00bbf7b5b8292ad363b5ce34a07359b14
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-10-09 21:29:01 +0700

    Форма

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
                 <ul>
                     <li><a href="/lab1">Первая лабораторная</a></li>
                     <li><a href="/lab2">Вторая лабораторная</a></li>
-                    <li><a href="/lab3">Вторая лабораторная</a></li>
+                    <li><a href="/lab3">Третья лабораторная</a></li>
                 </ul>
             </nav>
             <footer>
--- lab3.py
+++ lab3.py
@@ -4,18 +4,18 @@ lab3 = Blueprint('lab3', __name__)
 # Вывод cookie на странице
 @lab3.route('/lab3/')
 def lab3_page():
-    name = request.cookies.get('name', 'None')  # Получение cookie name
-    name_color = request.cookies.get('name_color', 'black')  # Получение цвета имени
+    name = request.cookies.get('name', 'None')  
+    name_color = request.cookies.get('name_color', 'black')  
     return render_template('lab3/lab3.html', name=name, name_color=name_color)
 
 
 # Установка cookies
 @lab3.route('/lab3/cookie')
 def set_cookie():
-    resp = make_response(redirect('/lab3/'))  # Редирект на страницу /lab3/
-    resp.set_cookie('name', 'Alex')  # Установка cookie name
-    resp.set_cookie('age', '20')  # Установка cookie age
-    resp.set_cookie('name_color', 'magenta')  # Установка cookie цвета имени
+    resp = make_response(redirect('/lab3/'))  
+    resp.set_cookie('name', 'Alex')  
+    resp.set_cookie('age', '20')  
+    resp.set_cookie('name_color', 'magenta')  
     return resp
 
 
@@ -27,3 +27,12 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+
