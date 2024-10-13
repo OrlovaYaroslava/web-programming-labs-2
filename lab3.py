f017889f1b24a7a49aeb3d5dcae48812f28d5a08
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-10-13 23:53:46 +0700

    Отчиска кук

--- lab3.py
+++ lab3.py
@@ -143,3 +143,20 @@ def ticket():
 
     return render_template('lab3/ticket_form.html')
 
+
+@lab3.route('/lab3/clear_settings')
+def clear_settings():
+    # Создаем ответ и удаляем все куки, установленные в приложении
+    resp = make_response(redirect('/lab3/settings'))
+    # Удаление кук, установленных в настройках
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('bold')
+    # Удаление остальных кук
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    
+    return resp
+
