3c59f3fb1f81c919205615eb75d3f632d70c6d62
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-11-24 18:27:59 +0700

    Доп_задание

--- lab6.py
+++ lab6.py
@@ -1,21 +1,46 @@
-from flask import Blueprint, request, jsonify, session, render_template
+from flask import Blueprint, request, jsonify, session, render_template, current_app
+import psycopg2
+import sqlite3
 
 lab6 = Blueprint('lab6', __name__)
 
-# Список офисов с добавлением стоимости аренды
-offices = [{"number": i, "tenant": "", "price": 900 + i * 3} for i in range(1, 11)]
+# Функция подключения к базе данных
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='yaroslava_orlova_knowledge_base',
+            user='yaroslava_orlova_knowledge_base',
+            password='123'
+        )
+        cur = conn.cursor()
+    else:
+        conn = sqlite3.connect('database.db')
+        cur = conn.cursor()
+    return conn, cur
+
+# Закрытие соединения с базой
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 @lab6.route('/lab6/')
 def main():
     return render_template('lab6/lab6.html')
 
+# Список офисов берется из таблицы
 @lab6.route('/lab6/json-rpc-api/', methods=['POST'])
 def api():
+    conn, cur = db_connect()
     data = request.json
     request_id = data.get('id', None)
-    
+
     # Метод "info" для получения списка офисов
     if data.get('method') == 'info':
+        cur.execute("SELECT number, tenant, price FROM offices ORDER BY number ASC")
+        offices = [{"number": row[0], "tenant": row[1], "price": row[2]} for row in cur.fetchall()]
+        db_close(conn, cur)
         return jsonify({
             "jsonrpc": "2.0",
             "result": offices,
@@ -25,6 +50,7 @@ def api():
     # Проверка авторизации
     login = session.get('login')
     if not login:
+        db_close(conn, cur)
         return jsonify({
             "jsonrpc": "2.0",
             "error": {"code": 1, "message": "Вы не авторизованы, пожалуйста, войдите в систему"},
@@ -34,46 +60,69 @@ def api():
     # Метод "booking" для бронирования офиса
     if data.get('method') == 'booking':
         office_number = data.get('params')
-        for office in offices:
-            if office['number'] == office_number:
-                if office['tenant']:
-                    return jsonify({
-                        "jsonrpc": "2.0",
-                        "error": {"code": 2, "message": "Офис уже забронирован"},
-                        "id": request_id
-                    })
-                office['tenant'] = login
-                return jsonify({
-                    "jsonrpc": "2.0",
-                    "result": "success",
-                    "id": request_id
-                })
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("SELECT tenant FROM offices WHERE number = %s", (office_number,))
+        else:
+            cur.execute("SELECT tenant FROM offices WHERE number = ?", (office_number,))
+        tenant = cur.fetchone()[0]
+
+        if tenant:
+            db_close(conn, cur)
+            return jsonify({
+                "jsonrpc": "2.0",
+                "error": {"code": 2, "message": "Офис уже забронирован"},
+                "id": request_id
+            })
+
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("UPDATE offices SET tenant = %s WHERE number = %s", (login, office_number))
+        else:
+            cur.execute("UPDATE offices SET tenant = ? WHERE number = ?", (login, office_number))
+        db_close(conn, cur)
+        return jsonify({
+            "jsonrpc": "2.0",
+            "result": "success",
+            "id": request_id
+        })
 
     # Метод "cancellation" для отмены брони
     if data.get('method') == 'cancellation':
         office_number = data.get('params')
-        for office in offices:
-            if office['number'] == office_number:
-                if not office['tenant']:
-                    return jsonify({
-                        "jsonrpc": "2.0",
-                        "error": {"code": 2, "message": "Нельзя снять бронь с неарендованного офиса"},
-                        "id": request_id
-                    })
-                if office['tenant'] != login:
-                    return jsonify({
-                        "jsonrpc": "2.0",
-                        "error": {"code": 3, "message": "Вы не можете снять бронь, так как офис арендован другим пользователем"},
-                        "id": request_id
-                    })
-                office['tenant'] = ""
-                return jsonify({
-                    "jsonrpc": "2.0",
-                    "result": "success",
-                    "id": request_id
-                })
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("SELECT tenant FROM offices WHERE number = %s", (office_number,))
+        else:
+            cur.execute("SELECT tenant FROM offices WHERE number = ?", (office_number,))
+        tenant = cur.fetchone()[0]
+
+        if not tenant:
+            db_close(conn, cur)
+            return jsonify({
+                "jsonrpc": "2.0",
+                "error": {"code": 2, "message": "Нельзя снять бронь с неарендованного офиса"},
+                "id": request_id
+            })
+
+        if tenant != login:
+            db_close(conn, cur)
+            return jsonify({
+                "jsonrpc": "2.0",
+                "error": {"code": 3, "message": "Вы не можете снять бронь, так как офис арендован другим пользователем"},
+                "id": request_id
+            })
+
+        if current_app.config['DB_TYPE'] == 'postgres':
+            cur.execute("UPDATE offices SET tenant = NULL WHERE number = %s", (office_number,))
+        else:
+            cur.execute("UPDATE offices SET tenant = NULL WHERE number = ?", (office_number,))
+        db_close(conn, cur)
+        return jsonify({
+            "jsonrpc": "2.0",
+            "result": "success",
+            "id": request_id
+        })
 
     # Если метод не найден
+    db_close(conn, cur)
     return jsonify({
         "jsonrpc": "2.0",
         "error": {"code": -32601, "message": "Метод не найден"},
