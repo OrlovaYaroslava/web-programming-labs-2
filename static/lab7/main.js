17144c869e00b795e00ca28883880a4dbb54c8e0
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-12-08 19:29:45 +0700

    Вывод таблицы

---
+++ static/lab7/main.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fillFilmList();
+});
+
+function fillFilmList() {
+    fetch('/lab7/rest-api/films/')
+        .then(response => response.json())
+        .then(films => {
+            let tbody = document.getElementById('film-list');
+            tbody.innerHTML = '';
+
+            for (let i = 0; i < films.length; i++) {
+                let tr = document.createElement('tr');
+                let tdTitle = document.createElement('td');
+                let tdTitleRus = document.createElement('td');
+                let tdYear = document.createElement('td');
+                let tdActions = document.createElement('td');
+
+                tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title;
+                tdTitleRus.innerText = films[i].title_ru;
+                tdYear.innerText = films[i].year;
+
+                let editButton = document.createElement('button');
+                editButton.innerText = 'Редактировать';
+                let deleteButton = document.createElement('button');
+                deleteButton.innerText = 'Удалить';
+
+                tdActions.append(editButton);
+                tdActions.append(deleteButton);
+
+                tr.append(tdTitle);
+                tr.append(tdTitleRus);
+                tr.append(tdYear);
+                tr.append(tdActions);
+
+                tbody.append(tr);
+            }
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
+
+document.getElementById('add-film-button').addEventListener('click', function() {
+    // Логика для добавления нового фильма
+    alert('Добавление нового фильма');
+});
