bf09ea5fe4cd5bbe286ee05c60f268ad61be33df
Ярослава Орлова <orlovayaroslawa@yandex.ru>
2024-12-25 00:03:29 +0700

    испр_ошиб

--- rgz.py
+++ rgz.py
@@ -34,7 +34,7 @@ def db_connect():
         cur = conn.cursor()
     else:
         dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
+        db_path = path.join(dir_path, "database.bd")
         conn = sqlite3.connect(db_path)
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
